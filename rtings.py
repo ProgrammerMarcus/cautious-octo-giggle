51e6d7ee68217fdfd541eb827ffa8c9948e6d0ee
Marcus Haapasaari Lindgren <maha2016@studetn.miun.se>
2023-02-20 17:00:45 +0100

    Added method to get score for rtings

--- rtings.py
+++ rtings.py
@@ -28,3 +28,17 @@ def get_list(model: str):
     return reviews
 
 
+def get_score(model: str):
+    """
+    Get the already averaged score on a scale of 0-10, as a float.
+    The website separates the score into various categories, this is the average of these.
+    @param model: The model to get the score for.
+    """
+    soup = BeautifulSoup(requests.get(_get_page(model)).content, 'html.parser')
+    score = 0
+    scores = soup.select(".usage_card-usages .e-score_box-value")
+    for n in scores:
+        score += float(n.text)
+    return float(score) / len(scores)
+
+
