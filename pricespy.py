5904f39eb86b357dd34c5699cf3f151f9e8b83c9
Jon Persson <jope0926@student.miun.se>
2023-02-20 12:20:50 +0100

    Added a function that looks for product model id

--- pricespy.py
+++ pricespy.py
@@ -1,12 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
+import re
+
 
 def _get_product(model: str):
+    """
+    Matches the product model name with an id by using PriceSpy's search.
+    :param model: The model name.
+    :return: The product model id as a string.
+    """
     url = "https://pricespy.co.uk/search?search=" + model
-    print(url)
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    print(soup.h1)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tags = soup.find_all("a")
+    hrefs = [tag.get("href") for tag in tags if "--p" in tag.get("href")]
+    if hrefs:
+        result = re.findall(r'--p\d+', hrefs[0])
+        return result[0]
 
 
-_get_product("playstation")
+def _get_reviews():
+    pass
+
+
+print(_get_product("nintendo"))
