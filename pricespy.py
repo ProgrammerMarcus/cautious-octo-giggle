73fbfc2e7caf1ef47cc6e248dcb5a83843144587
Jon Persson <jope0926@student.miun.se>
2023-02-20 21:40:09 +0100

    Now has a function that returns a list of reviews

--- pricespy.py
+++ pricespy.py
@@ -1,6 +1,11 @@
 import re
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 def _get_product_page(model: str):
@@ -14,35 +19,58 @@ def _get_product_page(model: str):
     soup = BeautifulSoup(response.text, 'html.parser')
     tags = soup.find_all("a")
     hrefs = [tag.get("href") for tag in tags if "--p" in tag.get("href")]
-    if hrefs:
-        return hrefs[0]
+    return hrefs[0]
 
 
 def _get_reviews(product_page: str, amount: int):
     """
-
-    :param product_page:
+    Uses webdriver to retrieve product model reviews.
+    It continues to add reviews to the list until the amount limit is reached.
+    :param product_page: Product page of the selected product model.
     :param amount: The amount of reviews to get.
     :return: A list of reviews.
     """
-    # review_url = get_review_link(product_page)
-    # response = requests.get(review_url)
-    # soup = BeautifulSoup(response.text, 'html.parser')
-    # tags = soup.find_all(string=re.compile("£.*"))
-    # tags = soup.find_all("div", attrs={"data-test": True})
-    # for tag in tags:
-    #     print(tag)
-    # tag = soup.find_all(class_=re.compile("Reviews.*"))
-    # for d in soup.find_all("div"):
-    #     print(d.get('style'))
-    # pattern = r'::before\s*{\s*content:\s*"([^"]+)"\s*;}'
-    # pattern = r'::before\s*'
-    #
-    # print(re.search(pattern, response.text).group())
-    #
-    # reviews = []
-    reviews = []
-    return reviews
+    url = get_review_link(product_page)
+    options = webdriver.ChromeOptions()
+
+    # Creates a headless Chrome instance, runs in the background without displaying a visible window
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    # Locating the button with "Show more" as text
+    button = WebDriverWait(driver, 10) \
+        .until(ec.element_to_be_clickable((By.XPATH,
+                                           '//button[contains(@class, "BaseButton--") and span[contains(text(), '
+                                           '"Show more")]]')))
+
+    # Scroll to the button element to bring it into view
+    actions = ActionChains(driver)
+    actions.move_to_element(button).perform()
+
+    # Script that clicks the "Show More" button, which loads more user reviews
+    driver.execute_script("arguments[0].click();", button)
+
+    # Wait for the page to load the additional reviews
+    user_review_div = WebDriverWait(driver, 10).until(
+        ec.presence_of_all_elements_located((By.CSS_SELECTOR, 'div[id^="review-text-"]')))
+
+    reviews = set()
+    while len(reviews) < amount:
+        for review_div in user_review_div:
+            review = review_div.text
+            if review not in reviews:
+                reviews.add(review)
+                if len(reviews) >= amount:
+                    break
+
+        driver.execute_script("arguments[0].click();", button)
+
+        user_review_div = WebDriverWait(driver, 10).until(
+            ec.presence_of_all_elements_located((By.CSS_SELECTOR, 'div[id^="review-text-"]')))
+
+    driver.quit()
+    return list(reviews)[:amount]
 
 
 def get_review_link(product_page: str):
@@ -55,6 +83,14 @@ def get_review_link(product_page: str):
 
 
 def get_list(model: str, amount: int):
+    """
+    Returns a list with reviews of the size of the specified amount.
+    The returned reviews are those of the product model that was on
+    the top of the search results.
+    :param model: The product model to gather reviews for.
+    :param amount: The amount of reviews to gather.
+    :return: A list of reviews.
+    """
     return _get_reviews(_get_product_page(model), amount)
 
 
@@ -68,6 +104,3 @@ def get_score(review_link: str):
     response = requests.get(review_link)
     score = re.findall(r'[^rank ]>(\d.\d)<', response.text)[0]
     return float(score)
-
-
-# _get_reviews(_get_product_page("microsoft xbox"), 0)
