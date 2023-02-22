e0693c19aeb594b8ab7951bdff06d169457499d7
Marcus Haapasaari Lindgren <maha2016@studetn.miun.se>
2023-02-22 23:18:20 +0100

    Updated pricerunner and rtings scraper based on new requirments.

--- pricerunner.py
+++ pricerunner.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import re
+from bs4 import BeautifulSoup
 
 
 def _get_product(model: str):
@@ -38,7 +39,18 @@ def _get_reviews(product: int, amount: int):
     return reviews
 
 
-def get_list(model: str, amount: int):
+def _get_score_(model: str):
+    """
+    Get the already averaged score on a scale of 1-5, as a float.
+    @param model: The model to get the score for.
+    """
+    url = "https://www.pricerunner.com/results?q=" + model
+    response = requests.get(url)
+    score = re.findall(r'>(\d\.\d)<', response.text).pop(0)
+    return float(score)
+
+
+def _get_list_(model: str, amount: int):
     """
     Attempts to return the specified amount of reviews about the specified product model.
     The product model whose reviews are returned are the one that is first in the
@@ -50,14 +62,68 @@ def get_list(model: str, amount: int):
     return _get_reviews(_get_product(model), amount)
 
 
-def get_score(model: str):
+def get_reviews(url: str):
     """
-    Get the already averaged score on a scale of 1-5, as a float.
-    @param model: The model to get the score for.
+    Returns the reviews in the URL.
+    @param url: The URL with the reviews.
+    @return: A list of reviews.
+    """
+    parsed = json.loads(requests.get(url).text)
+
+    reviews = list()
+
+    for content in parsed["reviewContent"]:
+        review = content["subContent"]["reviewText"]
+        if review is not None:
+            list.append(reviews, review)
+
+    return reviews
+
+
+def get_score(url: str):
+    """
+    Returns the score as calculated from the reviews on a scale of 0-10
+    @param url: The URL with the reviews.
+    @return: The score as a floating point number on a scale of 0-10.
+    """
+    parsed = json.loads(requests.get(url).text)
+
+    score = 0.0
+    counter = 0
+
+    for content in parsed["reviewContent"]:
+        rating = content["subContent"]["rank"]
+        if rating is not None:
+            counter += 1
+            score += float(rating)
+
+    if counter > 0:
+        return (score / counter) / 0.5
+    else:
+        return -1
+
+
+def get_search(model: str):
+    """
+    Returns the "top 5" search results based on the model name.
+    Currently, creates URLs with 30 reviews, which is the maximum that can be gotten in a single request.
+    @param model: The model to "search" for.
+    @return: A list containing dictionaries containing the name and URL of the products.
     """
     url = "https://www.pricerunner.com/results?q=" + model
-    response = requests.get(url)
-    score = re.findall(r'>(\d\.\d)<', response.text).pop(0)
-    return float(score)
+    response = requests.get(url).text
+    soup = BeautifulSoup(response, 'html.parser')
 
+    names = soup.select("h3")
+    ids = re.findall(r'href="/pl/\d*-(\d*)/', response)[:5]
+    products = list()
 
+    for i in range(len(ids)):
+        item = {
+            "url": "https://www.pricerunner.com/public/social/content/review/productversions/merged/" + ids[i] +
+                   "/uk?offset=0&limit=" + str(30) + '&testFreak=true&section=prio',
+            "name": names[i].text,
+        }
+        products.append(item)
+
+    return products
--- rtings.py
+++ rtings.py
@@ -3,7 +3,7 @@ import json
 from bs4 import BeautifulSoup
 
 
-def _get_page(model: str):
+def _get_page_(model: str):
     """
     Uses the website's search to find the page with the product model's review.
     @param model: The model name.
@@ -13,28 +13,28 @@ def _get_page(model: str):
     return "https://www.rtings.com" + json.loads(requests.get(url).text)["data"][0]["url"]
 
 
-def get_list(model: str):
+def get_reviews(url: str):
     """
     Returns a list containing the review, and only the review.
-    @param model: The model to find the review of.
+    @param url: The URL for the page with the review.
     @return: A list containing the review.
     """
-    soup = BeautifulSoup(requests.get(_get_page(model)).content, 'html.parser')
+    soup = BeautifulSoup(requests.get(url).content, 'html.parser')
     reviews = list()
     review = ""
     for r in soup.select(".test_group-description > p"):
-        review = review + r.text
+        review += r.text
     reviews.insert(0, review)
     return reviews
 
 
-def get_score(model: str):
+def get_score(url: str):
     """
-    Get the already averaged score on a scale of 0-10, as a float.
+    Get the averaged score on a scale of 0-10, as a float.
     The website separates the score into various categories, this is the average of these.
-    @param model: The model to get the score for.
+    @param url: The URL containing the review.
     """
-    soup = BeautifulSoup(requests.get(_get_page(model)).content, 'html.parser')
+    soup = BeautifulSoup(requests.get(url).content, 'html.parser')
     score = 0
     scores = soup.select(".usage_card-usages .e-score_box-value")
     for n in scores:
@@ -42,3 +42,21 @@ def get_score(model: str):
     return float(score) / len(scores)
 
 
+def get_search(model: str):
+    """
+    Returns the "top 5" search results based on the model name.
+    @param model: The model to "search" for.
+    @return: A list containing dictionaries containing the name and URL of the products.
+    """
+
+    response = json.loads(requests.get("https://www.rtings.com/api/v1/pages?query=" + model).text)
+    products = list()
+    try:
+        for i in range(5):
+            item = {
+                "url": "https://www.rtings.com" + response["data"][i]["url"],
+                "name": response["data"][i]["name"],
+            }
+            products.append(item)
+    finally:
+        return products
